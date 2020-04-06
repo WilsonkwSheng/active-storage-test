b7a6291be24aa2efbe4949b528d424f550e5f6ff
wilson <wilsonkhoo93@gmail.com>
2020-04-06 20:22:37 +0800

    Add download and preview profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_picture
+
+  after_commit :add_default_cover, on: [:create, :update]
+
+  def add_default_cover
+    unless profile_picture.attached?
+      profile_picture.attach(io: File.open(Rails.root.join("app", "assets", "images", "user_default.png")), filename: 'user_default.png' , content_type: "image/png")
+    end
+  end
 end
